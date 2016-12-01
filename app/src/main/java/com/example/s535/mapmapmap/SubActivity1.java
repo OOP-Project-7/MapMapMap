b95052414dd523a2ec53a5cc95a7831369c3ad6d
minjunsz <minjunsz@postech.ac.kr>
2016-12-02 05:16:28 +0900

    2016.12.02.05.16

--- app/src/main/java/com/example/s535/mapmapmap/SubActivity1.java
+++ app/src/main/java/com/example/s535/mapmapmap/SubActivity1.java
@@ -1,78 +1,208 @@
 package com.example.s535.mapmapmap;
 
-import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
+public class SubActivity1 extends AppCompatActivity{
+    ImageView pngmap;
+    ImageView mainmap;
+    ImageView map1;
+    ImageView map2;
+    ImageView map3;
+    ImageView map4;
 
-/**
- * Created by S535 on 2016-11-04.
- */
-
-public class SubActivity1 extends Activity
-{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub1);
+        pngmap=(ImageView)findViewById(R.id.pngmap);
+        LayoutInflater inflater = (LayoutInflater)getSystemService(
+                Context.LAYOUT_INFLATER_SERVICE);
+        LinearLayout.LayoutParams params=new  LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.MATCH_PARENT);
+        LinearLayout formainmap = (LinearLayout)inflater.inflate(R.layout.activity_mainmap, null);
+        addContentView(formainmap,params);
 
-        ImageButton button=(ImageButton)findViewById(R.id.imageButton);
-        button.setOnClickListener(new View.OnClickListener() {
+        map1=new ImageView(SubActivity1.this);
+        map2=new ImageView(SubActivity1.this);
+        map3=new ImageView(SubActivity1.this);
+        map4=new ImageView(SubActivity1.this);
+        map1.setImageResource(R.drawable.map1);
+        map2.setImageResource(R.drawable.map2);
+        map3.setImageResource(R.drawable.map3);
+        map4.setImageResource(R.drawable.map4);
+        map1.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "액티비티 전환", Toast.LENGTH_LONG).show();
-
-                // 액티비티 전환 코드
-                Intent intent = new Intent(getApplicationContext(), MapsActivity1.class);
-
-                startActivity(intent);
+            public boolean onTouch(View v, MotionEvent event) {
+                ImageView w=(ImageView) v;
+                Bitmap bitmap = ((BitmapDrawable)w.getDrawable()).getBitmap();
+                int[] location=new int[2];
+                v.getLocationOnScreen(location);
+                int x=(int)event.getRawX()-location[0];
+                int y=(int)event.getRawY()-location[1];
+                float xpercent=(float)x/v.getWidth();
+                float ypercent=(float)y/v.getHeight();
+                int bx=(int)(xpercent*bitmap.getWidth());
+                int by=(int)(ypercent*bitmap.getHeight());
+                int transparency = ((bitmap.getPixel(bx,by) & 0xff000000) >> 24);
+                if(transparency!=0)
+                {
+                    Intent intent = new Intent(getApplicationContext(), MapsActivity1.class);
+                    startActivity(intent);
+                }
+                return false;
             }
         });
-
-        ImageButton button2=(ImageButton)findViewById(R.id.imageButton2);
-        button2.setOnClickListener(new View.OnClickListener() {
-
+        map2.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "액티비티 전환", Toast.LENGTH_LONG).show();
-
-                // 액티비티 전환 코드
-                Intent intent = new Intent(getApplicationContext(), MapsActivity2.class);
-                startActivity(intent);
+            public boolean onTouch(View v, MotionEvent event) {
+                ImageView w=(ImageView) v;
+                Bitmap bitmap = ((BitmapDrawable)w.getDrawable()).getBitmap();
+                int[] location=new int[2];
+                v.getLocationOnScreen(location);
+                int x=(int)event.getRawX()-location[0];
+                int y=(int)event.getRawY()-location[1];
+                float xpercent=(float)x/v.getWidth();
+                float ypercent=(float)y/v.getHeight();
+                int bx=(int)(xpercent*bitmap.getWidth());
+                int by=(int)(ypercent*bitmap.getHeight());
+                int transparency = ((bitmap.getPixel(bx,by) & 0xff000000) >> 24);
+                if(transparency!=0)
+                {
+                    Intent intent = new Intent(getApplicationContext(), MapsActivity2.class);
+                    startActivity(intent);
+                }
+                return false;
             }
         });
-
-        ImageButton button3=(ImageButton)findViewById(R.id.imageButton3);
-        button3.setOnClickListener(new View.OnClickListener() {
-
+        map3.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "액티비티 전환", Toast.LENGTH_LONG).show();
-
-                // 액티비티 전환 코드
-                Intent intent = new Intent(getApplicationContext(), MapsActivity3.class);
-                startActivity(intent);
+            public boolean onTouch(View v, MotionEvent event) {
+                ImageView w=(ImageView) v;
+                Bitmap bitmap = ((BitmapDrawable)w.getDrawable()).getBitmap();
+                int[] location=new int[2];
+                v.getLocationOnScreen(location);
+                int x=(int)event.getRawX()-location[0];
+                int y=(int)event.getRawY()-location[1];
+                float xpercent=(float)x/v.getWidth();
+                float ypercent=(float)y/v.getHeight();
+                int bx=(int)(xpercent*bitmap.getWidth());
+                int by=(int)(ypercent*bitmap.getHeight());
+                int transparency = ((bitmap.getPixel(bx,by) & 0xff000000) >> 24);
+                if(transparency!=0)
+                {
+                    Intent intent = new Intent(getApplicationContext(), MapsActivity3.class);
+                    startActivity(intent);
+                }
+                return false;
             }
         });
-
-        ImageButton button4=(ImageButton)findViewById(R.id.imageButton4);
-        button4.setOnClickListener(new View.OnClickListener() {
-
+        map4.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "액티비티 전환", Toast.LENGTH_LONG).show();
+            public boolean onTouch(View v, MotionEvent event) {
+                ImageView w=(ImageView) v;
+                Bitmap bitmap = ((BitmapDrawable)w.getDrawable()).getBitmap();
+                int[] location=new int[2];
+                v.getLocationOnScreen(location);
+                int x=(int)event.getRawX()-location[0];
+                int y=(int)event.getRawY()-location[1];
+                float xpercent=(float)x/v.getWidth();
+                float ypercent=(float)y/v.getHeight();
+                int bx=(int)(xpercent*bitmap.getWidth());
+                int by=(int)(ypercent*bitmap.getHeight());
+                int transparency = ((bitmap.getPixel(bx,by) & 0xff000000) >> 24);
+                if(transparency!=0)
+                {
+                    Intent intent = new Intent(getApplicationContext(), MapsActivity4.class);
+                    startActivity(intent);
+                }
+                return false;
+            }
+        });
+        addContentView(map1,params);
+        addContentView(map2,params);
+        addContentView(map3,params);
+        addContentView(map4,params);
 
-                // 액티비티 전환 코드
-                Intent intent = new Intent(getApplicationContext(), MapsActivity4.class);
-                startActivity(intent);
+        map1.setVisibility(View.INVISIBLE);
+        map2.setVisibility(View.INVISIBLE);
+        map3.setVisibility(View.INVISIBLE);
+        map4.setVisibility(View.INVISIBLE);
+
+        mainmap=(ImageView)findViewById(R.id.mainmap);
+
+        pngmap.setAlpha((float)0.5);
+        mainmap.setAlpha((float)0);
+
+        mainmap.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                ImageView w=(ImageView) v;
+                Bitmap bitmap = ((BitmapDrawable)w.getDrawable()).getBitmap();
+                int[] location=new int[2];
+                v.getLocationOnScreen(location);
+                int x=(int)event.getRawX()-location[0];
+                int y=(int)event.getRawY()-location[1];
+                float xpercent=(float)x/v.getWidth();
+                float ypercent=(float)y/v.getHeight();
+                int bx=(int)(xpercent*bitmap.getWidth());
+                int by=(int)(ypercent*bitmap.getHeight());
+                int transparency = ((bitmap.getPixel(bx,by) & 0xff000000) >> 24);
+                if(transparency!=0)
+                {
+                    if(bx>200&&bx<2500&&by<1300)
+                    {
+                        map1.setVisibility(View.INVISIBLE);
+                        map2.setVisibility(View.INVISIBLE);
+                        map3.setVisibility(View.INVISIBLE);
+                        map4.setVisibility(View.INVISIBLE);
+                        map4.setVisibility(View.VISIBLE);
+                    }
+                    if(bx>870&&bx<2500&&by>1300&&by<2500)
+                    {
+                        map1.setVisibility(View.INVISIBLE);
+                        map2.setVisibility(View.INVISIBLE);
+                        map3.setVisibility(View.INVISIBLE);
+                        map4.setVisibility(View.INVISIBLE);
+                        map3.setVisibility(View.VISIBLE);
+                    }
+                    if(bx>330&&bx<1700&&by>2500&&by<5400)
+                    {
+                        map1.setVisibility(View.INVISIBLE);
+                        map2.setVisibility(View.INVISIBLE);
+                        map3.setVisibility(View.INVISIBLE);
+                        map4.setVisibility(View.INVISIBLE);
+                        map1.setVisibility(View.VISIBLE);
+                    }
+                    if(bx>1800&&bx<2700&&by>2700&&by<5000)
+                    {
+                        map1.setVisibility(View.INVISIBLE);
+                        map2.setVisibility(View.INVISIBLE);
+                        map3.setVisibility(View.INVISIBLE);
+                        map4.setVisibility(View.INVISIBLE);
+                        map2.setVisibility(View.VISIBLE);
+                    }
+
+                }
+                else
+                {
+                    map1.setVisibility(View.INVISIBLE);
+                    map2.setVisibility(View.INVISIBLE);
+                    map3.setVisibility(View.INVISIBLE);
+                    map4.setVisibility(View.INVISIBLE);
+                }
+
+                return false;
             }
         });
     }
 }
-
